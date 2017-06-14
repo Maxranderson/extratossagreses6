984d0e4e2861fa4fb835c86cc05b12c257cb0db9
Maxranderson Araújo <max_rodrigues_araujo@hotmail.com>
2017-06-14 11:03:55 -0300

    Implementado a mudança da app

--- js/app/controllers/AppController.js
+++ js/app/controllers/AppController.js
@@ -1,10 +1,17 @@
 class AppController {
 
 	constructor() {
-
+		this._apps = document.querySelectorAll(".apps");
+		console.log(this._apps);
 	}
 
 	change(id){
-
+		this._apps.forEach(app => {
+			if(app.id == id){
+				app.classList.remove('hidden');
+			}else{
+				app.classList.add('hidden');
+			}
+		});
 	}
 }
---
+++ js/index.js
@@ -0,0 +1,1 @@
+var appController = new AppController();
